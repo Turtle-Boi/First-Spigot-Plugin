b2aac7e44cdd80ea83a08d4dcaf908425d309aa5
Corbinator333 <corbinator@live.com>
2023-06-20 21:41:25 -0700

    Added basic functionality

--- src/main/java/com/turtleboi/firstspigotplugin/First_Spigot_Plugin.java
+++ src/main/java/com/turtleboi/firstspigotplugin/First_Spigot_Plugin.java
@@ -1,5 +1,6 @@
 package com.turtleboi.firstspigotplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class First_Spigot_Plugin extends JavaPlugin {
@@ -7,11 +8,12 @@ public final class First_Spigot_Plugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+    Bukkit.getLogger().info("Hello World");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getLogger().info("Shutting down");
     }
 }
