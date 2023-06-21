dacd43206066a50406661c94e9f0d8cdd6c46c37
Corbinator333 <corbinator@live.com>
2023-06-20 21:11:36 -0700

    Initial commit

---
+++ src/main/java/com/turtleboi/firstspigotplugin/First_Spigot_Plugin.java
@@ -0,0 +1,17 @@
+package com.turtleboi.firstspigotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class First_Spigot_Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
